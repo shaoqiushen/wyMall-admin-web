12cc78ad8463ba107cf88714b86dc7cd0e74f7ab
shaoqiushen <to_shensq@126.com>
2019-03-28 17:20:29 +0800

    测试路由2

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '../pages/home/Home'
-import order from '../pages/order/Order'
+const home = () => import('../pages/home/Home')
+const order = () => import('../pages/order/Order')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: home,
-      redirect: '/home'
+      path: '/', name: 'home', component: home, redirect: '/home'
     },
     {
-      path: '/home',
-      name: 'home',
-      component: home
+      path: '/home', name: 'home', component: home
     },
     {
-      path: '/order',
-      name: 'order',
-      component: order
+      path: '/order', name: 'order', component: order
     }
   ]
 })
